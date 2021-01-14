f3212ebb4f149292adf079df00ab75d780204690
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-01-14 18:15:42 -0500

    add more custom Routes

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const port = process.env.PORT || 3000
 app.get('/',(req,res) => {
     res.send("Hello World from Rajeshwari Rudravaram's app!")
 })
-
+app.get('/about', (req, res) => {
+    res.send(' You have requested the about page! ')
+  })
+app.get('/contact', (req, res) => {
+    res.send(' You have requested the contact page! ')
+  })
+app.get('/help', (req, res) => {
+    res.send(' You have requested the help page! ')
+  })
+app.get('/help/:topic', (req, res) => {
+    res.send(` You have requested the help ${req.params.topic} page! `)
+  })
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
     
