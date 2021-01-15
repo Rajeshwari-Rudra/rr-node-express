fd011e21558c332956d48b1163760a849600c72d
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-01-15 11:18:51 -0500

    changed response messages on routes

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/',(req,res) => {
-    res.send("Hello World from Rajeshwari Rudravaram's app!")
+    res.send("Hello World Rajeshwari Rudravaram's app!")
 })
 app.get('/about', (req, res) => {
-    res.send(' You have requested the about page! ')
+    res.send(' This app is an example of node express app.')
   })
 app.get('/contact', (req, res) => {
-    res.send(' You have requested the contact page! ')
+    res.send('If you would like to contact me,please email me at S538361@nwmissouri.edu')
   })
 app.get('/help', (req, res) => {
-    res.send(' You have requested the help page! ')
+    res.send(" You have requested the help page of this app, I'm Rajeshwari Rudravaram will help you soon.")
   })
 app.get('/help/:topic', (req, res) => {
     res.send(` You have requested the help ${req.params.topic} page! `)
