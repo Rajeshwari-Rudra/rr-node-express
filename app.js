a734e5dbf2389bd74a5209d001ce17a9d5773203
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-01-15 11:39:29 -0500

    updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/',(req,res) => {
     res.send("Hello World Rajeshwari Rudravaram's app!")
 })
 app.get('/about', (req, res) => {
-    res.send(' This app is an example of node express app.')
+    res.send(' This is an example of node express app.')
   })
 app.get('/contact', (req, res) => {
     res.send('If you would like to contact me,please email me at S538361@nwmissouri.edu')
@@ -15,7 +15,7 @@ app.get('/help', (req, res) => {
     res.send(" You have requested the help page of this app, I'm Rajeshwari Rudravaram will help you soon.")
   })
 app.get('/help/:topic', (req, res) => {
-    res.send(` You have requested the help ${req.params.topic} page! `)
+    res.send(` You have asked help from  ${req.params.topic} page! `)
   })
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
